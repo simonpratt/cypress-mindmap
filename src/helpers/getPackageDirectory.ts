c9b3c9244acd954f0370da08794b6ff36011a7fb
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-07-10 20:51:31 +0930

    Removing path of helper

--- src/helpers/getPackageDirectory.ts
+++ src/helpers/getPackageDirectory.ts
@@ -2,7 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 // const __dirname = path.resolve();
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url)).replace('/dist/helpers', '');
 
 export const getPackageDirectory = () => {
   console.log('package dir', __dirname);
