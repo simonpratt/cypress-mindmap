a05ca4bc89cbd80f46bfbc394e35874bf5b737c2
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-07-10 20:28:35 +0930

    Using correct path for static page files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 export const start = () => {
   const app = express();
 
   app.use('/json', express.static('TEMP_MINDMAP'));
-  app.use('/', express.static('dist/ui'));
+  app.use('/', express.static(`${__dirname}/dist/ui`));
 
   app.listen(5112);
 };
