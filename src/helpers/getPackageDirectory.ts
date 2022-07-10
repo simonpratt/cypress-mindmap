c69287cebb2ac42803a1943e042e5a7acc69c298
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-07-10 20:48:32 +0930

    Changed dirname function

--- src/helpers/getPackageDirectory.ts
+++ src/helpers/getPackageDirectory.ts
@@ -1,6 +1,8 @@
-import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const getPackageDirectory = () => {
   console.log('package dir', __dirname);
