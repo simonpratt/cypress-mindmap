1fe455a9ca0ea437842af91678e85cedbb9264ad
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-07-10 20:32:42 +0930

    Debugging

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
 
-const __dirname = path.resolve();
+const __fake = path.resolve();
 
 export const start = () => {
   const app = express();
+
+  console.log('real dir name', __dirname);
+  console.log('fake dir name', __fake);
 
   app.use('/json', express.static('TEMP_MINDMAP'));
   app.use('/', express.static(`${__dirname}/dist/ui`));
